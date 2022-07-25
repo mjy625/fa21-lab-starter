819d7b2ba3bcfebb3f6c97333db2f5c1f853be48
mjy625 <2546432235@qq.com>
2022-07-25 18:28:41 +0800

    proj1

---
+++ fa21-proj1-starter/src/hashtable.c
@@ -0,0 +1,111 @@
+#include "hashtable.h"
+
+/*
+ * General utility routines (including malloc()).
+ */
+#include <stdlib.h>
+
+/*
+ * Standard IO and file routines.
+ */
+#include <stdio.h>
+
+/*
+ * String utility routines.
+ */
+#include <string.h>
+
+/*
+ * This creates a new hash table of the specified size and with
+ * the given hash function and comparison function.
+ */
+HashTable *createHashTable(int size, unsigned int (*hashFunction)(void *),
+                           int (*equalFunction)(void *, void *)) {
+  int i = 0;
+  HashTable *newTable = malloc(sizeof(HashTable));
+  if (NULL == newTable) {
+    fprintf(stderr, "malloc failed \n");
+    exit(1);
+  }
+  newTable->size = size;
+  newTable->buckets = malloc(sizeof(struct HashBucketEntry *) * size);
+  if (NULL == newTable->buckets) {
+    fprintf(stderr, "malloc failed \n");
+    exit(1);
+  }
+  for (i = 0; i < size; i++) {
+    newTable->buckets[i] = NULL;
+  }
+  newTable->hashFunction = hashFunction;
+  newTable->equalFunction = equalFunction;
+  return newTable;
+}
+
+/* Task 1.2 */
+void insertData(HashTable *table, void *key, void *data) {
+  // -- TODO --
+  // HINT:
+  // 1. Find the right hash bucket location with table->hashFunction.
+  // 2. Allocate a new hash bucket entry struct.
+  // 3. Append to the linked list or create it if it does not yet exist. 
+  int index=table->hashFunction(key),size=table->size;
+  HashBucketEntry* newEntry=malloc(sizeof (HashBucketEntry));
+  newEntry->key=key;
+  newEntry->data=data;
+  newEntry->next=NULL;
+  HashBucketEntry* p=table->buckets[index%size];
+  if(p==NULL)
+    table->buckets[index%size]=newEntry;
+  else{
+    HashBucketEntry* prev;
+    while(p!=NULL){
+      prev=p;
+      p=p->next;
+    }
+    prev->next=newEntry;
+  }
+}
+
+/* Task 1.3 */
+void *findData(HashTable *table, void *key) {
+  // -- TODO --
+  // HINT:
+  // 1. Find the right hash bucket with table->hashFunction.
+  // 2. Walk the linked list and check for equality with table->equalFunction.
+  int index=table->hashFunction(key),size=table->size;
+  if(table->buckets[index%size]==NULL)return NULL;
+  HashBucketEntry* p=table->buckets[index%size];
+  while(p!=NULL&&table->equalFunction(p,key))p=p->next;
+  if(p!=NULL)return p->data;
+  else return NULL;
+}
+
+/* Task 2.1 */
+unsigned int stringHash(void *s) {
+  // -- TODO --
+  //fprintf(stderr, "need to implement stringHash\n");
+  /* To suppress compiler warning until you implement this function, */
+  char* p_str=(char*)s;
+  unsigned int hash_value=0;
+  for(int i=0;i<strlen(p_str);++i){
+    hash_value+=(int)p_str[i]*(i+1);
+  }
+  return hash_value;
+}
+
+/* Task 2.2 */
+int stringEquals(void *s1, void *s2) {
+  // -- TODO --
+  //fprintf(stderr, "You need to implement stringEquals");
+  /* To suppress compiler warning until you implement this function */
+  char* p1=(char*)s1;
+  char* p2=(char*)s2;
+  if(strlen(p1)!=strlen(p2))
+    return 0;
+  while(*p1){
+    if(*p1!=*p2)
+      return 0;
+    p1++,p2++;
+  }
+  return 1;
+}
---
+++ fa21-proj1-starter/src/philphix.c
@@ -0,0 +1,156 @@
+/*
+ * Include the provided hash table library.
+ */
+#include "hashtable.h"
+
+/*
+ * Include the header file.
+ */
+#include "philphix.h"
+
+/*
+ * Standard IO and file routines.
+ */
+#include <stdio.h>
+
+/*
+ * General utility routines (including malloc()).
+ */
+#include <stdlib.h>
+
+/*
+ * Character utility routines.
+ */
+#include <ctype.h>
+
+/*
+ * String utility routines.
+ */
+#include <string.h>
+
+/*
+ * This hash table stores the dictionary.
+ */
+HashTable *dictionary;
+
+/*
+ * The MAIN routine.  You can safely print debugging information
+ * to standard error (stderr) as shown and it will be ignored in 
+ * the grading process.
+ */
+#ifndef _PHILPHIX_UNITTEST
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Specify a dictionary\n");
+    return 1;
+  }
+  /*
+   * Allocate a hash table to store the dictionary.
+   */
+  fprintf(stderr, "Creating hashtable\n");
+  dictionary = createHashTable(0x61C, &stringHash, &stringEquals);
+
+  fprintf(stderr, "Loading dictionary %s\n", argv[1]);
+  readDictionary(argv[1]);
+  fprintf(stderr, "Dictionary loaded\n");
+
+  fprintf(stderr, "Processing stdin\n");
+  processInput();
+
+  /*
+   * The MAIN function in C should always return 0 as a way of telling
+   * whatever program invoked this that everything went OK.
+   */
+  return 0;
+}
+#endif /* _PHILPHIX_UNITTEST */
+
+/* Task 3 */
+void readDictionary(char *dictName) {
+  // -- TODO --
+  fprintf(stderr, "You need to implement the varible length\n");
+  FILE* pfile =  fopen(dictName,"r+");
+  if(pfile==NULL){
+    fprintf(stderr,"Can't open the file");
+    exit(61);
+  }
+  const int fix_size=60;
+  while(1){
+    char* key=malloc(fix_size);
+    char* value=malloc(fix_size);
+    fscanf(pfile,"%s",key);
+    fscanf(pfile,"%s",value);
+    if(key[0]==EOF||key[0]==' '||key[0]=='\0')
+      break;
+    insertData(dictionary,(void*)key,(void*)value);
+  }
+}
+
+int isAlpha(char ch){
+  return (ch>='0'&&ch<='9')
+    || (ch>='a'&&ch<='z')
+    || (ch>='A'&&ch<='Z');
+}
+
+int replace(){
+  int max_size=60;
+  char* str=malloc(max_size);
+  char buffer='\0';
+  int i=0,isEnd=0;
+  while(1){
+    for(i;i<max_size;++i){
+      char ch;
+      rewind(stdin);
+      scanf("%c",&ch);
+      if(ch==EOF||ch=='?')
+        isEnd=1;
+      if(isAlpha(ch))
+        str[i]=ch;
+      else{
+        str[i]='\0';
+        buffer=ch;
+        break;
+      }
+    }
+    if(i==max_size&&str[max_size-1]!='\0')
+      str=realloc(str,max_size*2);
+    else
+      break;
+  }
+  char* value=findData(dictionary,str);
+  char* origin=malloc(strlen(str));
+  strcpy(origin,str);
+  if(value!=NULL)
+    printf("%s",value);
+  else{
+    for(int i=1;i<strlen(str);++i){
+      if(str[i]>='A'&&str[i]<='Z')
+        str[i]+='a'-'A';
+    }
+    value=findData(dictionary,str);
+    if(value!=NULL)
+      printf("%s",value);
+    else if (str[0]>='A'&&str[0]<='Z')
+    {
+      str[0]+='a'-'A';
+      value=findData(dictionary,str);
+      if(value!=NULL)
+        printf("%s",value);
+      else printf("%s",origin);
+    }
+    else printf("%s",origin);
+  }
+
+  if(buffer!='\0')
+    printf("%c",buffer);
+  return isEnd;
+}
+
+/* Task 4 */
+void processInput() {
+  // -- TODO --
+  //fprintf(stderr, "You need to implement processInput\n");
+  while(1)
+    if(replace()==1)
+      return;
+}
